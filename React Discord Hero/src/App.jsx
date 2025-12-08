cf676e5ce16909def4c0badb3dbbe6f25ac95c4e
David Bouhaben <davidbouhaben@yahoo.co.id>
2025-12-08 23:00:32 +0700

    feat: update App component with Discord logo and menu icon, enhance layout with navigation

--- React Discord Hero/src/App.jsx
+++ React Discord Hero/src/App.jsx
@@ -1,13 +1,31 @@
 // import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
+import discordLogo from "./assets/discord-logo-white.png";
+import menuIcon from "./assets/menu-icon.png";
 // import viteLogo from '/vite.svg'
 import "./App.css";
 
 function App() {
+  let title = "Imagine A Place...".toUpperCase();
+  let information = `...where you can belong to a school club, a gaming group, or a
+              worldwide art community. Where just you and a handful of friends
+              can spend time together. A place that makes it easy to talk every
+              day and hang out more often.
+`;
+
   return (
     <>
       <div className="App">
-        
+        <header>
+          <nav className="nav-discord">
+            <img src={discordLogo} alt="The logo of Discord" />
+            <img src={menuIcon} alt="The icon of a menu" />
+          </nav>
+
+          <div className="info">
+            <h1>{title}</h1>
+            <p>{information}</p>
+          </div>
+        </header>
       </div>
     </>
   );
